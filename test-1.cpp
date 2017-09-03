987dfcbf1bee4c3212512b900a425571b12251c3
scamacho <scamacho@dc.uba.ar>
2017-09-03 20:36:46 -0300

    funcionando el ej 1 sin pthreads

--- test-1.cpp
+++ test-1.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 
 int main(void) {
-	Lista<pair<string, unsigned int>> *l;
-	pair<string, unsigned int> p = make_pair("hola manola", 99);
-
-	l->push_front(p);
+	Lista<pair<string, unsigned int>> *l= new Lista<pair<string, unsigned int>>;
+	pair<string, unsigned int> p1 = make_pair("(primer push_front) pos:", 3);
+	pair<string, unsigned int> p2 = make_pair("(segundo push_front) pos:", 2);
+	pair<string, unsigned int> p3 = make_pair("(tercer push_front) pos:", 1);
+	l->push_front(p1);
+	l->push_front(p2);
+	l->push_front(p3);
 
 	for (auto it = l->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
 		auto t = it.Siguiente();
