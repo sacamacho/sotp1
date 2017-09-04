f98080d1ec4b6b2b00043c651b8cf102406ddbf6
scamacho <scamacho@dc.uba.ar>
2017-09-03 23:38:39 -0300

    test del 1 completo, peor da con rc

--- test-1.cpp
+++ test-1.cpp
@@ -21,18 +21,21 @@ void *pushea_front(void *lAP)
 
 int main(void) {
 
-	// cout << "----LISTA SIN PTHREADS----" << endl;
+	cout << "----LISTA SIN PTHREADS----" << endl;
+	Lista<pair<string, unsigned int>> *l0= new Lista<pair<string, unsigned int>>;
+	pair<string, unsigned int> p1 = make_pair("(primer push_front) pos:", 3);
+	pair<string, unsigned int> p2 = make_pair("(segundo push_front) pos:", 2);
+	pair<string, unsigned int> p3 = make_pair("(tercer push_front) pos:", 1);
+	l0->push_front(p1);
+	l0->push_front(p2);
+	l0->push_front(p3);
+	for (auto it = l0->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
+		auto t = it.Siguiente();
+		cout << t.first << " " << t.second << endl;
+	}
+
 	Lista<pair<string, unsigned int>> *l= new Lista<pair<string, unsigned int>>;
-	// pair<string, unsigned int> p1 = make_pair("(primer push_front) pos:", 3);
-	// pair<string, unsigned int> p2 = make_pair("(segundo push_front) pos:", 2);
-	// pair<string, unsigned int> p3 = make_pair("(tercer push_front) pos:", 1);
-	// l->push_front(p1);
-	// l->push_front(p2);
-	// l->push_front(p3);
-	// for (auto it = l->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
-	// 	auto t = it.Siguiente();
-	// 	cout << t.first << " " << t.second << endl;
-	// }
+
 
 	cout << "----LISTA CON PTHREADS----" << endl;
 	pthread_t thread[CANT_THREADS]; int tid;
