65d8f49c4866cfd5f44befe1f5334ef56e2723c9
aferenaz <31170974+aferenaz@users.noreply.github.com>
2017-09-05 10:32:19 -0300

    Add files via upload

--- ConcurrentHashMap.hpp
+++ ConcurrentHashMap.hpp
@@ -5,13 +5,13 @@
 #include <vector>
 #include <string>
 #include <cstring>
-//#include <utility>
+#include <utility>
 #include <list>
 #include <iostream>
-//#include <fstream>
-//#include <mutex> 
+#include <fstream>
+#include <mutex> 
 #include <pthread.h>
-//#include <stdio.h>
+#include <stdio.h>
 #include "ListaAtomica.hpp"
 
 #define DIMENSION_TABLA 26
@@ -19,25 +19,25 @@
 using namespace std;
 
 class ConcurrentHashMap {
-	private:		
-		unsigned int calculoPosicion(const char letra){return (int)letra - 97;}
-
-	public:
+   public:
 		ConcurrentHashMap(); //constructor
-		~ConcurrentHashMap();
+		//*~ConcurrentHashMap();*/
 		void addAndInc(string key); 
 		bool member(string key);
 		pair<string, unsigned int> maximum(unsigned int nt);
-
 		vector< Lista < pair<string, unsigned int> >* > tabla;
-		
+		static ConcurrentHashMap count_words(string arch);
+	private:		
+		unsigned int calculoPosicion(const char letra){return (int)letra - 97;}
+		pthread_mutex_t mutex[DIMENSION_TABLA];
+		pthread_mutex_t mutex_maximum;
+
+	
 		// lo de arriba es de la clase, lo de abajo no
 		
 
         // void* cargoHashMap(void *thread_args); // en count_words necesita esto
 		// solicita la funcion con 3 argumentos diferentes, es así?
-		
-		// ConcurrentHashMap count_words(string arch); //Ejercicio 2
 	 //    ConcurrentHashMap count_words(list<string> archs); //Ejercicio 3
 		// ConcurrentHashMap count_words(unsigned int n, list<string> archs); // Ejercicio 4
 		// pair<string, unsigned int> maximum(unsigned int p_archivos, unsigned int p_maximos, list<string>archs); // Ejercicio 5
